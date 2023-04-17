e5a1e0e215a4306c60ce949b9104e3fe544b7069
Rafał Gałecki <darthunter@gmail.com>
2023-04-17 11:52:59 +0200

    review links parsing bug fixed

--- src/components/ReviewsElement/ReviewsElement.jsx
+++ src/components/ReviewsElement/ReviewsElement.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import Markdown from 'marked-react';
 import { ReviewLi } from './ReviewsElement.styled';
-
+import parse from 'html-react-parser';
 const ReviewsElement = ({ author, content }) => {
+  const parsed = parse(content);
   return (
     <ReviewLi>
       <h4>Author: {author}</h4>
-      <Markdown>{content}</Markdown>
+      {content.includes('<em>') ? (
+        <p>{parsed}</p>
+      ) : (
+        <Markdown>{content}</Markdown>
+      )}
     </ReviewLi>
   );
 };
-
 export default ReviewsElement;
